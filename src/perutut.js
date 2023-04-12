e94fae6d5b84702044d63631eb972bb9b0c71ec4
Alex Inkiläinen <alexinkilainen@gmail.com>
2023-04-12 21:37:38 +0300

    Vaihdettu peruttujen vuorojen poistoaika kahteen tuntiin

--- src/perutut.js
+++ src/perutut.js
@@ -73,9 +73,9 @@ async function tarkistaPerutut (tila) {
       var departureTimeNum = Number(perututVuorot[i].scheduledDeparture)
       var serviceDayNum = Number(perututVuorot[i].serviceDay)
 
-      // var alertEndDate = departureTimeNum + 10800 + moment().unix();
-      var alertEndDate = departureTimeNum + 10800 + serviceDayNum
-      // var alertEndDate = departureTimeNum + 180 + serviceDayNum;
+      var alertEndDate = departureTimeNum + 7200 + serviceDayNum  // Lisää kaksi tuntia lähtöajan päälle tietokantaa varten
+      // var alertEndDate = departureTimeNum + 180 + serviceDayNum // Kolme minuuttia (testausta varten)
+
       // Viesti
       let lahetettavaViesti
 
