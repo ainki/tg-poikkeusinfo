c87418311ca37abf34bec3360979a3757204f0a4
Alex Inkiläinen <alexinkilainen@gmail.com>
2023-04-12 15:29:43 +0300

    Lisätty selitykset configiin
    Nämäkin voisi melkeempä siirtää enviin

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     // Modules
-    enablePerutut: true,
-    enablePoikkeukset: true,
-    enableDebug: false,
+    enablePerutut: true, //Perutut vuorot päälle/pois
+    enablePoikkeukset: true, //Poikkeusinfo päälle/pois
+    enableDebug: false, // Polling päälle/pois
     // API
     digitransitAPILink: 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql?digitransit-subscription-key=' + process.env.digitransitApiKey,
     // Telegram channels
