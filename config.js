2901318240e5fc3c416935e5e58d67b0c333efde
Alex Inkiläinen <alexinkilainen@gmail.com>
2023-09-28 12:46:37 +0300

    Debug and Polling separated

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports = {
   // Modules
   enablePerutut: true, // Perutut vuorot päälle/pois
   enablePoikkeukset: true, // Poikkeusinfo päälle/pois
-  enableDebug: false, // Polling päälle/pois
+  enablePolling: true, // Polling päälle/pois
+  enableDebug: true,
   // API
   digitransitAPILink: 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql?digitransit-subscription-key=' + process.env.digitransitApiKey,
   // Telegram channels
