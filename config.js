525f753485200aeb0aa7715b70f5bab9f9f63f60
Alex Inkiläinen <alexinkilainen@gmail.com>
2023-10-03 11:44:11 +0300

    Polling off by default

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Modules
   enablePerutut: true, // Perutut vuorot päälle/pois
   enablePoikkeukset: true, // Poikkeusinfo päälle/pois
-  enablePolling: true, // Polling päälle/pois
+  enablePolling: false, // Polling päälle/pois
   enableDebug: true,
   // API
   digitransitAPILink: 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql?digitransit-subscription-key=' + process.env.digitransitApiKey,
